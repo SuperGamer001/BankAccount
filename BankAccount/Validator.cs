7e08cd236a8666dce07a29372b9f7580fadd2f82
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-08-14 13:49:12 -0700

    Added a bad implementation of IsWithinRange()

---
+++ BankAccount/Validator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class Validator
+    {
+        /// <summary>
+        /// Checks to ensure a value is between inclusive boundaries.
+        /// </summary>
+        /// <param name="value">Value to check</param>
+        /// <param name="min">Minimum Inclusive Boundaries</param>
+        /// <param name="max">Maximum Inclusive Boundaries</param>
+        /// <returns></returns>
+        public bool IsWithinRange(double value, double min, double max)
+        {
+            //return value >= min && value <= max;
+            if (value < min)
+            {
+                return true;
+            }
+            else if (value > max)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
